8df221e74e3de9f3974b65d6fb41d2f867bf8f6b
blazer1400 <70761510+blazer1400@users.noreply.github.com>
2021-12-02 15:55:11 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+	$('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+	$(window).resize(function(){
+        $('#homeFullScreen').css({height:$(window).height(),width:$(window).width()});
+    });
+
+});
+$(document).ready(function() {
+    $('#portfolio-container').css({height:$(window).height(),width:$(window).width()});
+    $(window).resize(function(){
+        $('#portfolio-container').css({height:$(window).height(),width:$(window).width()});
+    });
+
+});
+
+var aboutme = document.getElementById("aboutme");
+var myskills = document.getElementById("myskills");
+var mywork = document.getElementById("mywork");
+var contact = document.getElementById("contact");
+
+function show(article) {
+    switch (article) {
+        case "aboutme":
+            myskills.style.opacity = "0";
+            mywork.style.opacity = "0";
+            contact.style.opacity = "0";
+            myskills.style.display = "none";
+            mywork.style.display = "none";
+            contact.style.display = "none";
+
+            aboutme.style.display = "block";
+            setTimeout(function() {
+                aboutme.style.opacity = "1";
+            }, 400);
+            break;
+        case "myskills":
+            mywork.style.display = "none";
+            mywork.style.opacity = "0";
+            contact.style.display = "none";
+            contact.style.opacity = "0";
+            aboutme.style.display = "none";
+            aboutme.style.opacity = "0";
+
+            myskills.style.display = "block";
+            setTimeout(function() {
+                myskills.style.opacity = "1";
+            }, 400);
+            break;
+        case "mywork":
+            myskills.style.display = "none";
+            myskills.style.opacity = "0";
+            contact.style.display = "none";
+            contact.style.opacity = "0";
+            aboutme.style.display = "none";
+            aboutme.style.opacity = "0";
+
+            mywork.style.display = "block";
+            setTimeout(function() {
+                mywork.style.opacity = "1";
+            });
+            break;
+        case "contact":
+            mywork.style.display = "none";
+            mywork.style.opacity = "0";
+            myskills.style.display = "none";
+            myskills.style.opacity = "0";
+            aboutme.style.display = "none";
+            aboutme.style.opacity = "0";
+
+            contact.style.display = "block";
+            setTimeout(function() {
+                contact.style.opacity = "1";
+            }, 400);
+            break;
+    }
+}
